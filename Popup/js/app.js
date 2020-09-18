6ce435dabe1d960287f88abb5a1d29613a955913
Beyda <50250602+beydacar1@users.noreply.github.com>
2020-09-18 20:13:13 +0300

    Add files via upload

---
+++ Popup/js/app.js
@@ -0,0 +1,14 @@
+pageOpen();
+
+function pageOpen() {
+    setTimeout(() => {
+        document.getElementById("popup").style.display = "block"
+    }, 4000);
+}
+
+
+document.getElementById("close").addEventListener("click", () => {
+    if (document.getElementById("popup").style.display = "block")
+        document.getElementById("popup").style.display = "none"
+});
+
